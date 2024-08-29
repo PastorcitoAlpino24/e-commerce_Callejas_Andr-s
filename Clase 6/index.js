28f4a9a05e53a466ae354759e11c73c2eee01657
PastorcitoAlpino24 <andrescalllejas204@gmail.com>
2024-08-29 20:08:14 -0300

    clase6

---
+++ Clase 6/index.js
@@ -0,0 +1,21 @@
+console.log(1);
+console.log(1+1==2);
+console.log("Hola");
+console.log(1.1+0.2);
+console.log('Hola');
+console.log("hola");
+
+variable ="Hola, ¿cómo estás? Fui a comprar unas sodas y no te encontré."; //variable global
+console.log(variable);
+
+let name = "Shrek"; //let: declaración de variable. Permite reasignar el valor.
+name = 4;
+name;
+
+const dog = "Max"; //idem pero no se puede reasignar.
+
+const array = ["leche", "maíz"];
+array.push("pokémon");
+console.log(array);
+
+console.log(variable == name);
