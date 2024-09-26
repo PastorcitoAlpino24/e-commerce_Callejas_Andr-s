3f2923b9f15dbd1172f4b88e80562d8837103f8d
PastorcitoAlpino24 <andrescalllejas204@gmail.com>
2024-09-26 19:43:54 -0300

    Clase10

--- e-commerce_Callejas_Andrés/index.js
+++ e-commerce_Callejas_Andrés/index.js
@@ -1,23 +1,28 @@
-const h1 = document.querySelector("h1");
-const section = document.querySelector("section");
+function vermas() {
+    const h1 = document.querySelector("h1");
+    const section = document.querySelector("section");
 
-h1.innerText = "Productos";
+    h1.innerText = "Productos";
 
-let array = [];
+    let array = [];
 
-for (let i = 1; i < 10; i++) {
-    array.push(`<div class="card-group">
-                    <div class="card">
-                        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    for (let i = 1; i < 10; i++) {
+        array.push(`<div class="card-group">
+                        <div class="card">
+                            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <button class="btn btn-primary">Ver más</button>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-                </div>`);
+                    </div>`);
+    }
+
+    section.innerHTML = array.join("");
 }
 
-section.innerHTML = array.join("");
+vermas();
