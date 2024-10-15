2e8f0bbfba0c4f8fb8c9d22bad0e91e712c8ed2c
PastorcitoAlpino24 <andrescalllejas204@gmail.com>
2024-10-15 19:26:29 -0300

    clase objetos arreglo

--- e-commerce_Callejas_Andrés/producto.js
+++ e-commerce_Callejas_Andrés/producto.js
@@ -8,20 +8,19 @@ class Producto {
     }
 }
 
-let objeto = new Producto("producto", "detalle", 100, 100, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
+let objeto = new Producto("Producto", "Detalle del producto", 100, 100, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
 
 const etiquetasHTML = `
     <div class="card">
-        <img src="${productoSeleccionado.imagen}" class="card-img-top" alt="${productoSeleccionado.titulo}">
+        <img src="${objeto.imagen}" class="card-img-top" alt="${objeto.titulo}">
         <div class="card-body">
-            <h5 class="card-title">${productoSeleccionado.titulo}</h5>
-            <p class="card-text">${productoSeleccionado.detalle}</p>
-            <p class="card-text"><strong>Precio:</strong> $${productoSeleccionado.precio}</p>
-            <p class="card-text"><strong>Stock disponible:</strong> ${productoSeleccionado.stock}</p>
+            <h5 class="card-title">${objeto.titulo}</h5>
+            <p class="card-text">${objeto.detalle}</p>
+            <p class="card-text"><strong>Precio:</strong> $${objeto.precio}</p>
+            <p class="card-text"><strong>Stock disponible:</strong> ${objeto.stock}</p>
         </div>
     </div>
     `;
-
 
 const main = document.querySelector("main");
 main.innerHTML = etiquetasHTML;
