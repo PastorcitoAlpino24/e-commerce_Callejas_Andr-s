a535286cc9586549014e5e9de944c14c6ab1685a
PastorcitoAlpino24 <andrescalllejas204@gmail.com>
2024-10-16 19:54:09 -0300

    clase 15 metodos

--- e-commerce_Callejas_Andrés/sprint 2/producto.js
+++ e-commerce_Callejas_Andrés/sprint 2/producto.js
@@ -1,4 +1,4 @@
-class Producto {
+/* class Producto {
     constructor(titulo, detalle, precio, stock, imagen) {
         this.titulo = titulo;
         this.detalle = detalle;
@@ -7,20 +7,43 @@ class Producto {
         this.imagen = imagen;
     }
 }
+*/
 
-let objeto = new Producto("Producto", "Detalle del producto", 100, 100, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
+//let objeto = new Producto("Producto", "Detalle del producto", 100, 100, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
 
-const etiquetasHTML = `
-    <div class="card">
-        <img src="${objeto.imagen}" class="card-img-top" alt="${objeto.titulo}">
-        <div class="card-body">
-            <h5 class="card-title">${objeto.titulo}</h5>
-            <p class="card-text">${objeto.detalle}</p>
-            <p class="card-text"><strong>Precio:</strong> $${objeto.precio}</p>
-            <p class="card-text"><strong>Stock disponible:</strong> ${objeto.stock}</p>
+const elemento = window.location.search.split("=")[1];
+
+const data = [
+    { "id": 1, "title": "Gate of Flesh (Nikutai no mon)", "detail": "Phasellus sit amet erat. Nulla tempus.", "img": "http://dummyimage.com/165x100.png/dddddd/000000", "price": 6, "stock": 37 },
+    { "id": 2, "title": "Robin Hood", "detail": "Nullam sit amet turpis elementum ligula vehicula consequat.", "img": "http://dummyimage.com/221x100.png/cc0000/ffffff", "price": 95, "stock": 50 },
+    { "id": 3, "title": "Streaks, The (Pregi)", "detail": "Fusce consequat. Nulla nisl. Nunc nisl.", "img": "http://dummyimage.com/244x100.png/ff4444/ffffff", "price": 7, "stock": 41 },
+    { "id": 4, "title": "Sea Hawk, The", "detail": "In quis justo. Maecenas rhoncus aliquam lacus.", "img": "http://dummyimage.com/120x100.png/dddddd/000000", "price": 33, "stock": 22 },
+    { "id": 5, "title": "Four Sided Triangle", "detail": "Duis bibendum, felis sed interdum venenatis.", "img": "http://dummyimage.com/195x100.png/dddddd/000000", "price": 26, "stock": 86 },
+    { "id": 6, "title": "Fantastic Night, The (Nuit fantastique, La)", "detail": "Duis aliquam convallis nunc.", "img": "http://dummyimage.com/102x100.png/ff4444/ffffff", "price": 14, "stock": 54 },
+    { "id": 7, "title": "Beau Brummel", "detail": "Fusce posuere felis sed lacus.", "img": "http://dummyimage.com/150x100.png/ff4444/ffffff", "price": 56, "stock": 63 },
+    { "id": 8, "title": "Acacia", "detail": "Phasellus sit amet erat.", "img": "http://dummyimage.com/240x100.png/5fa2dd/ffffff", "price": 33, "stock": 32 },
+    { "id": 9, "title": "Winchester '73", "detail": "Proin eu mi. Nulla ac enim.", "img": "http://dummyimage.com/242x100.png/5fa2dd/ffffff", "price": 36, "stock": 89 }
+];
+
+const productoFiltrado = data.find(producto => producto.id == elemento);
+
+if (productoFiltrado) {
+    const etiquetasHTML = `
+        <div class="card">
+            <img src="${productoFiltrado.img}" class="card-img-top" alt="${productoFiltrado.title}">
+            <div class="card-body">
+                <h5 class="card-title">${productoFiltrado.title}</h5>
+                <p class="card-text">${productoFiltrado.detail}</p>
+                <p class="card-text"><strong>Precio:</strong> $${productoFiltrado.price}</p>
+                <p class="card-text"><strong>Stock disponible:</strong> ${productoFiltrado.stock}</p>
+            </div>
         </div>
-    </div>
     `;
 
-const main = document.querySelector("main");
-main.innerHTML = etiquetasHTML;
+    const main = document.querySelector("main");
+    main.innerHTML = etiquetasHTML;
+    
+} else {
+    const main = document.querySelector("main");
+    main.innerHTML = "<p>Producto no encontrado.</p>";
+}
--- e-commerce_Callejas_Andrés/sprint 2/script.js
+++ e-commerce_Callejas_Andrés/sprint 2/script.js
@@ -1,31 +1,33 @@
-function vermas() {
-    const h1 = document.querySelector("h1");
-    const section = document.querySelector("section");
+const data = [
+    { "id": 1, "title": "Gate of Flesh (Nikutai no mon)", "detail": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.", "img": "http://dummyimage.com/165x100.png/dddddd/000000", "price": 6, "stock": 37 },
+    { "id": 2, "title": "Robin Hood", "detail": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", "img": "http://dummyimage.com/221x100.png/cc0000/ffffff", "price": 95, "stock": 50 },
+    { "id": 3, "title": "Streaks, The (Pregi)", "detail": "Fusce consequat. Nulla nisl. Nunc nisl.", "img": "http://dummyimage.com/244x100.png/ff4444/ffffff", "price": 7, "stock": 41 },
+    { "id": 4, "title": "Sea Hawk, The", "detail": "In quis justo. Maecenas rhoncus aliquam lacus.", "img": "http://dummyimage.com/120x100.png/dddddd/000000", "price": 33, "stock": 22 },
+    { "id": 5, "title": "Four Sided Triangle", "detail": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi.", "img": "http://dummyimage.com/195x100.png/dddddd/000000", "price": 26, "stock": 86 },
+    { "id": 6, "title": "Fantastic Night, The (Nuit fantastique, La)", "detail": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.", "img": "http://dummyimage.com/102x100.png/ff4444/ffffff", "price": 14, "stock": 54 },
+    { "id": 7, "title": "Beau Brummel", "detail": "Fusce posuere felis sed lacus.", "img": "http://dummyimage.com/150x100.png/ff4444/ffffff", "price": 56, "stock": 63 },
+    { "id": 8, "title": "Acacia", "detail": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.", "img": "http://dummyimage.com/240x100.png/5fa2dd/ffffff", "price": 33, "stock": 32 },
+    { "id": 9, "title": "Winchester '73", "detail": "Proin eu mi. Nulla ac enim.", "img": "http://dummyimage.com/242x100.png/5fa2dd/ffffff", "price": 36, "stock": 89 }
+];
 
-    h1.innerText = "Productos";
+const section = document.querySelector("section");
 
-    let array = [];
-
-    for (let i = 1; i < 10; i++) {
-
-        array.push(`<div class="card-group">
-            <div class="card">
-                <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title ${i}</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <button class="btn btn-primary" onclick="location.href='producto.html';">Ver más</button>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
+let array = data.map(producto => `
+    <div class="card-group">
+        <div class="card">
+            <img src="${producto.img}" class="card-img-top" alt="${producto.title}">
+            <div class="card-body">
+                <h5 class="card-title">${producto.title}</h5>
+                <p class="card-text">${producto.detail}</p>
+                <p class="card-text"><strong>Precio:</strong> $${producto.price}</p>
+                <p class="card-text"><strong>Stock disponible:</strong> ${producto.stock}</p>
+                <button class="btn btn-primary" onclick="location.href='producto.html?prod=${producto.id}';">Ver más</button>
             </div>
-        </div>`);
-    }
+            <div class="card-footer">
+                <small class="text-body-secondary">Última actualización hace 3 minutos</small>
+            </div>
+        </div>
+    </div>
+`);
 
-    section.innerHTML = array.join("");
-}
-
-vermas();
-
-
+section.innerHTML = array.join("");
