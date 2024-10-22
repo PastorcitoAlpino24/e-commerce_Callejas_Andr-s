5d2e015b3c0df66a41b596ad9a613e766222bfe2
PastorcitoAlpino24 <andrescalllejas204@gmail.com>
2024-10-22 20:41:32 -0300

    clase 21 login

---
+++ e-commerce_Callejas_Andrés/sprint 2/login.js
@@ -0,0 +1,23 @@
+const sessionAccount = {
+    email: "luckytroll@hotmail.com",
+    password: "furby"
+}
+
+const form = document.querySelector("form");
+
+// console.dir(form.elements.Email);
+
+form.addEventListener ("submit",  (event) => {
+    event.preventDefault();
+    let psswd = form.elements.password.value; //value = name del input
+    let email = form.elements.email.value;
+
+    if (sessionAccount.email === email && sessionAccount.password === psswd){
+        window.location.href = "./index.html";
+        localStorage.setItem("email", email);
+    } else{
+        alert("Datos incorrectos");
+        form.elements.password.value = "";
+        form.elements.email.value = "";
+    }
+});
